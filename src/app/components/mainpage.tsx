3ccf564a8fc99cb0bf599df2fae9ab21f543f4a9
taufan.zamzami <taufan.zamzami@jamiis-macbook.local>
2025-01-06 14:10:18 +0700

    added poppins font (actually) and fixed most unused imports, refined landing page animation

--- src/app/components/mainpage.tsx
+++ src/app/components/mainpage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useAnimation } from 'framer-motion'
 
 
 function mainpage() {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import LandingPage from './components/landingpage'
-import Header from './components/header'
-import MainPage from './components/mainpage'
-import { Main } from 'next/document';
+
 
 
 const Page = () => {
   return (
     <div>
-     <Header />
      <LandingPage />
     </div>
   );
