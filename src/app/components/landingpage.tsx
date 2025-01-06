d60d484ce16f2241e1d5e04b0a3bd95960cbd15a
taufan.zamzami <taufan.zamzami@jamiis-macbook.local>
2025-01-06 18:02:03 +0700

    added mainpage and animated headers

--- src/app/components/landingpage.tsx
+++ src/app/components/landingpage.tsx
@@ -4,14 +4,13 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 function LandingPage() {
-  // Text Logic
   const [firstControl, setFirstControl] = useState('hidden');
   const [secondControl, setSecondControl] = useState('hidden');
   const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
-    const firstTextDuration = 3000; 
-    const secondTextDelay = firstTextDuration + 1000; 
+    const firstTextDuration = 1500; 
+    const secondTextDelay = firstTextDuration + 750; 
 
     setFirstControl('visible');
 
@@ -33,7 +32,7 @@ function LandingPage() {
   return (
     <div className="relative w-full overflow-hidden h-screen">
       <div className="w-full h-screen">
-        <div className="fixed inset-0">
+        <div className="inset-0">
           <motion.div
             className="absolute w-full h-full"
             style={{
@@ -43,36 +42,36 @@ function LandingPage() {
             }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 1, ease: 'easeInOut' }}
+            transition={{ duration: 0.8, ease: 'easeInOut' }} 
           />
         </div>
       </div>
       <div>
       <motion.div
-      className="fixed inset-0 flex justify-center items-center"
-      variants={{
-        hidden: {left: 0},
-        visible: {left: "100%"}
-      }}
-      initial="hidden"
-      animate="visible"
-      transition={{
-        duration: 0.5,
-        ease: "easeIn"
-      }}
-      style={{
-        position: "absolute",
-        top: 4,
-        bottom: 4,
-        left: 0,
-        right: 0,
-        background: "#1f2937",
-        zIndex: 20,
-      }}
+        className="fixed inset-0 flex justify-center items-center"
+        variants={{
+          hidden: {left: 0},
+          visible: {left: "100%"}
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{
+          duration: 0.6, 
+          ease: "easeIn"
+        }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          background: "#1f2937",
+          zIndex: 20,
+        }}
       >
       </motion.div>
       <motion.div
-        className="fixed inset-0 flex justify-center items-center"
+        className="absolute inset-0 flex justify-center items-center"
         variants={{
           hidden: {
             opacity: 0,
@@ -83,28 +82,27 @@ function LandingPage() {
           visible: {
             opacity: 1,
             y: 0,
-            transition: { duration: 1, delay: 0.2, ease: 'easeInOut' }, 
+            transition: { duration: 0.8, delay: 0.2, ease: 'easeInOut' }, 
             filter: "blur(0px)",
           },
           fadeOut: {
             opacity: 0,
             y: -50,
-            transition: { duration: 0.8, delay: 0.5, ease: 'easeInOut' }, 
+            transition: { duration: 0.6, delay: 0.3, ease: 'easeInOut' }, 
             filter: "blur(33px)",
           }
         }}
         initial="hidden" 
         animate={firstControl}
-        >
+      >
         <p className="text-white font-poppins font-extrabold text-center mt-4 text-2xl sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl">
-          Hello, I&apos;m Muhammad Fatih Zamzami
+          Welcome to My Digital Playground
         </p>
       </motion.div>
-        </div>
-
+      </div>
 
       <motion.div
-        className="fixed inset-0 flex justify-center items-center"
+        className="absolute inset-0 flex justify-center items-center"
         variants={{
           hidden: {
             opacity: 0,
@@ -115,19 +113,18 @@ function LandingPage() {
           visible: {
             opacity: 1,
             y: 0,
-            transition: { duration: 1, delay: 0.2, ease: 'easeInOut' }, 
+            transition: { duration: 0.8, delay: 0.2, ease: 'easeInOut' }, 
             filter: "blur(0px)",
           },
         }}
         initial="hidden"
         animate={secondControl}
       >
-        <p className="text-white font-poppins font-extrabold text-center mt-4 text-2xl sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl">
-          Welcome to my journey
+        <p className="absolute text-white font-poppins font-superbold text-center mt-4 text-2xl sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl">
+          Step into My World of Discovery
         </p>
       </motion.div>
 
-    
       {showButton && (
         <motion.div
           className="absolute bottom-8 left-0 right-0 flex justify-center items-center"
@@ -140,12 +137,12 @@ function LandingPage() {
             y: 0, 
             filter: "blur(0px)"}}
           transition={{
-            duration: 1.5, 
+            duration: 0.8, 
             ease: 'easeOut',
-            delay: 0.3 
+            delay: 0.2 
           }}
         >
-          <button className="bg-gray-800 text-white hover:bg-white hover:text-gray-800 font-poppins font-extrabold py-3 px-6 rounded-full text-xl font-semibold shadow-lg backdrop-blur-sm hover:bg-black/90 transition-colors">
+          <button className="bg-gray-800 text-white hover:bg-white hover:text-gray-800 font-poppins font-extrabold py-3 px-6 rounded-full text-xl shadow-lg backdrop-blur-sm hover:bg-black/90 transition-colors">
             Discover More
           </button>
         </motion.div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,63 @@
-import LandingPage from './components/landingpage'
-
-
+'use client';
+import { useRef, useEffect } from 'react';
+import { motion, useInView, useAnimation } from 'framer-motion';
+import Header from './components/header';
+import LandingPage from './components/landingpage';
+import MainPage from './components/mainpage';
 
 const Page = () => {
+  const mainPageRef = useRef(null);
+  const isInView = useInView(mainPageRef, { 
+    amount: 0.5, 
+    once: false
+  });
+  const headerControl = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      headerControl.start("visible")
+    } else{
+      headerControl.start("hidden")
+    }
+  }, [isInView, headerControl]);
+
   return (
     <div>
-     <LandingPage />
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+            x: 20,
+            transition: {
+            duration: 0.5,
+            ease: "easeOut",
+          }},
+          visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+              duration: 1.5,
+              ease: "easeOut"
+            }}
+      }}        
+        initial={{ opacity: 0, y: 20 }}
+        animate={headerControl}
+        style={{ 
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 50 
+        }}
+      >
+        <Header />
+      </motion.div>
+      <LandingPage />
+      <div ref={mainPageRef}>
+        <MainPage />
+      </div>
     </div>
   );
 };
 
-export default Page
+export default Page;
