baa81d8b7f2ab3830ccd2b6ffec2a9c32b407e1d
taufan.zamzami <taufan.zamzami@jamiis-macbook.local>
2025-01-07 20:56:14 +0700

    fixed a bug in the reveal file

--- src/app/animations/reveal.tsx
+++ src/app/animations/reveal.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
+'use client';
+import React, { useEffect, useRef } from "react";
 import { motion, useInView, useAnimation } from "framer-motion";
 
 interface Props {
